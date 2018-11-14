4be05419708f6fe6ae021fdfb13b50cbade3d1ed
Luigi Maestrelli <lgmaestrelli@gmail.com>
2018-11-13 22:06:41 -0200

    Navbar without auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 
 class App extends Component {
-  render() {
-    return (
-        <BrowserRouter>
-            <div className="App">
-                <h1>MyPlan</h1>
-            </div>
-        </BrowserRouter>      
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    <Navbar></Navbar>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
